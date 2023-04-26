1fdf407f36f44d556cee6352e1a81325f41f8dad
MaharramAhmadli <127186172+MaharramAhmadli@users.noreply.github.com>
2023-04-26 22:42:45 +0400

    Update main.js

--- JS Arrays/assets/js/main.js
+++ JS Arrays/assets/js/main.js
@@ -1,4 +1,19 @@
-   alert();
-   const restaurantarray = ["kfc", "mcdonalds", "papa johns", "50 qepik"];
-   restaurantarray.length = restaurantarray.length -1;
-   console.log(restaurantarray); 
+// Array-a dərsdə keçdiyimizə uyğun remove funksiyası yazın. Bu method Arrayın sonuncu elementini silsin.
+1-ci usul
+
+//    alert();
+//    const restaurantarray = ["kfc", "mcdonalds", "papa johns", "50 qepik"];
+//    restaurantarray.length = restaurantarray.length -1;
+//    console.log(restaurantarray); 
+
+
+2-ci usul
+
+// let restaurant = {
+//    arr : ["kfc", "mcdonalds", "papa johns", "50 qepik"];
+//    remove : fuction (){
+//       this.arr.length --
+//    }
+// }
+// restaurant.remove()
+// alert(restaurant.arr);
